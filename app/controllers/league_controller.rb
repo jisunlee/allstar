252d281d3ac7931bbc9f6d6055c94fc72ccd3271
jisunlee <soyou213@gmail.com>
2013-12-04 11:30:54 +0900

    Get contect from Question

--- app/controllers/league_controller.rb
+++ app/controllers/league_controller.rb
@@ -1,9 +1,16 @@
 class LeagueController < ApplicationController
   def quiz 　
-    #問題を取りに行く処理
+    ##問題を取りに行く処理
+    
+    #セッションが空ではないこと確認
+    unless session[:q_no].blank?
+    
+      #セッション情報から質問問題を取得しビューに渡す
+      @question_content = Question.find(session[:q_no]).try(:content).presence
+    end
   end
   
   def
-  　#回答を受け取る処理
+    #回答を受け取る処理
   end      
 end
